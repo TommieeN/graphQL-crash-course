daac647a5fbd62d32227716acf395ffe86d13b54
TommyNguyen <timsnguyen@hotmail.com>
2023-12-10 16:44:10 -0800

    add resolver functions

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+import db from './_db.js'
+
 import { typeDefs } from './schema.js'
+
+// Note function names need to MATCH schema names
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        }
+    }
+}
+
+
+
 // server setup
 const server = new ApolloServer({
     // typeDefs -- definitions of types of data EX. game, author, etc.
     typeDefs,
-    // resolvers 
+    resolvers,
 })
 
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 }
 })
 
-console.log('Server readt at port', 4000)
+console.log('Server ready at port', 4000)
